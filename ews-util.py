af216475d61091ce84905919c7285dbb46a8ddcf
kashev <kashev.dalmia@gmail.com>
2014-04-18 13:51:42 -0500

    working pretty printing

--- ews-util.py
+++ ews-util.py
@@ -9,6 +9,7 @@
 # IMPORTS
 import urllib2
 import json
+import os
 
 # EWS API VARIALBLES
 # if the EWS API changes, these too might need to change.
@@ -20,10 +21,19 @@ lab_string   = 'strlabname'
 
 resp = urllib2.urlopen(ews_url)
 html = resp.read()
+
 json_response = json.loads(html)
 data = json_response[data_string]
-# print json.dumps(data, sort_keys=True, indent=2, separators=(',', ': '))
+
+# Pretty Printing
+maxlen_lab = max([len(lab[lab_string]) for lab in data])
+maxlen_use = max([len(str(lab[use_string])) for lab in data])
+maxlen_tot = max([len(str(lab[total_string])) for lab in data])
 
 for lab in data:
-    print lab[lab_string], ' : ', lab[use_string], '/', lab[total_string]
+    print lab[lab_string].ljust(maxlen_lab, ' '), \
+          ':', \
+          str(lab[use_string]).ljust(maxlen_use, ' '), \
+          '/', \
+          str(lab[total_string]).ljust(maxlen_tot, ' ')
 
